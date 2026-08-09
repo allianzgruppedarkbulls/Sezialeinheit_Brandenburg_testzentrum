d0f889034030f17b335ab08d6cec306bd2bc9834
Spezialeinheit Brandenburg <allianzgruppe.darkbulls@gmail.com>
2026-08-10 01:40:54 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Diese Datei kannst du in GitHub Pages sicher veröffentlichen.
 window.BULLS_CONFIG = {
   // Google Apps Script WebApp-URL (als API für Login/Chat/Admin)
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxBWPevVgRENi4_afYssRKsAmHeLD8LIDpXVm1Upd5_mE1wYy09CfQtc5i_jje0jJQysg/exec",
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycby1R27XHQ86CLQWipsq1zKw27MS9TKQOjckxJMhGNJ-SpYaRNS9ZUNgDlswiv5sS841ag/exec",
 }
 
